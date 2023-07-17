3064c517cde218a78bbff22b9e7c78146761a5ef
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-07-17 14:23:39 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/jsx-key */
 import { useDispatch, useSelector } from "react-redux";
 // import { fetchTodos } from "./redux/slice/todo";
 import "./App.css";
